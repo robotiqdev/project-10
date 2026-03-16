0854088eb0fe6c21410268d591400e1785eea5fa
Sandbox <sandbox@factory>
2026-03-16 21:21:53 +0000

    feat: TASK-4508 - Create Input Struct and Parser Orchestrator

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,7 +1,28 @@
 package parser
 
+import "strconv"
+
 // Parse parses the provided args slice into an *Input.
 // args must contain exactly three elements: [left, operator, right].
 func Parse(args []string) (*Input, error) {
-	return nil, nil
+	if err := ValidateArgCount(args); err != nil {
+		return nil, err
+	}
+
+	op := args[1]
+	if err := ValidateOperator(op); err != nil {
+		return nil, err
+	}
+
+	if err := ValidateNumber(args[0]); err != nil {
+		return nil, err
+	}
+	left, _ := strconv.ParseFloat(args[0], 64)
+
+	if err := ValidateNumber(args[2]); err != nil {
+		return nil, err
+	}
+	right, _ := strconv.ParseFloat(args[2], 64)
+
+	return &Input{Left: left, Right: right, Operator: op}, nil
 }
