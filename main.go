370471d6adb3da9ee3bd775d61afff9525fccb36
Sandbox <sandbox@factory>
2026-03-16 23:12:24 +0000

    feat: TASK-4514 - Create CLI Integration Tests

--- main.go
+++ main.go
@@ -1,3 +1,32 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+
+	"github.com/example/calc-app/internal/calc"
+	"github.com/example/calc-app/internal/parser"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	input, err := parser.Parse(args)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	engine := calc.NewEngine()
+	result, err := engine.Calculate(calc.Operation{
+		Left:     input.Left,
+		Operator: input.Operator,
+		Right:    input.Right,
+	})
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	fmt.Fprintf(os.Stdout, "%s %s %s = %s\n", args[0], args[1], args[2], result)
+}
